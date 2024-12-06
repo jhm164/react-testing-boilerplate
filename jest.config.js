321af88a450e87bc3c563f0fe30a406f63caf652
jhm16 <saurabhsolkar4@gamil.com>
2024-12-06 15:12:51 +0530

    first

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.css$': '<rootDir>/__mocks__/styleMock.js',  // Mock CSS imports
+  },
+}
+
---
+++ src/test/app.test.js
@@ -0,0 +1,7 @@
+import App from "../App";
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(3).toBe(3);
+  });
+  
+  
